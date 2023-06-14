1ec5e7a0a8f542a0d01fca381b3cacda283f4843
Kitajima Akira <kitajima@osakac.ac.jp>
2023-06-14 16:45:01 +0900

    コメントを追加。

--- dom2017b/dom2017b.cpp
+++ dom2017b/dom2017b.cpp
@@ -12,16 +12,20 @@ enum class judge_result {IDENTICAL, CLOSE, DIFFERENT};
 
 // 二つの文字列の違いを判定する。
 judge_result judge(const string& s1, const string& s2) {
-	bool one_mistake = false;
-	auto p1 = s1.begin(), p2 = s2.begin();
+	// 各文字列の先頭から順に確認する。
+	bool one_mistake = false;  // 一つ目の違いを見つけたかどうか。
+	auto p1 = s1.begin(), p2 = s2.begin();  // 現在確認中の文字の位置。
 	for (bool in_string = false; p1 != s1.end() && p2 != s2.end(); ++p1, ++p2) {
-		if (*p1 == '"' && *p2 == '"') {
+		if (*p1 == '"' && *p2 == '"') {  // 文字列の区切りか?
 			in_string = !in_string;
 			continue;
 		}
-		if (*p1 != *p2) {
+		if (*p1 != *p2) {  // 違う文字か?
+			// 違っていると判定できるか確認する。
 			if (!in_string || one_mistake)
 				return judge_result::DIFFERENT;
+
+			// 文字列中なので、文字列の終わりまで読み飛ばす。
 			one_mistake = true;
 			while (*p1 != '"')
 				++p1;
@@ -30,15 +34,16 @@ judge_result judge(const string& s1, const string& s2) {
 			in_string = false;
 		}
 	}
-	if (p1 != s1.end() || p2 != s2.end())
+
+	// どちらかの文字列が終わってからの判定処理を行う。
+	if (p1 != s1.end() || p2 != s2.end())  // もう一方がまだ残っている?
 		return judge_result::DIFFERENT;
-	if (one_mistake)
-		return judge_result::CLOSE;
-	return judge_result::IDENTICAL;
+	return one_mistake ? judge_result::CLOSE : judge_result::IDENTICAL;
 }
 
 int main() {
-	for (string s1, s2; cin >> s1, s1 != "."; ) {
+	for (string s1; cin >> s1 && s1 != "."; ) {
+		string s2;
 		cin >> s2;
 		switch (judge(s1, s2)) {
 		case judge_result::IDENTICAL:
