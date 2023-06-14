d78a25d1e08f12bddd538a36f44de80633a65fde
Kitajima Akira <kitajima@osakac.ac.jp>
2023-06-14 18:16:08 +0900

    関数を使う形に変更。

--- dom2018a/dom2018a.cpp
+++ dom2018a/dom2018a.cpp
@@ -5,25 +5,29 @@
 
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <vector>
 using namespace std;
 
+// 所得データ1セットに対して解を得る。
+int get_answer(const vector<int>& incomes) {
+	// 合計値を計算する。
+	int sum = accumulate(incomes.begin(), incomes.end(), 0);  // 所得の合計値
+	// 所得の平均値を計算する。
+	double average = static_cast<double>(sum) / incomes.size();
+	// 平均以上の人数を計算する。
+	return count_if(incomes.begin(), incomes.end(), [&](int a) {return a <= average; });
+}
+
 int main() {
 	for (int n; cin >> n && n > 0; ) {
-		// 入力を得て、合計値も計算しておく。
+		// 入力を得る。
 		vector<int> incomes(n);  // 所得のリスト
-		
-		int sum = 0;  // 所得の合計値
-		for (auto& a : incomes) {
+		for (auto& a : incomes)
 			cin >> a;
-			sum += a;
-		}
-
-		// 所得の平均値を計算する。
-		double average = static_cast<double>(sum) / n;
 
 		// 条件を満たす人数を数えて出力する。
-		cout << count_if(incomes.begin(), incomes.end(), [=](int a) {return a <= average; }) << endl;
+		cout << get_answer(incomes) << endl;
 	}
 	return 0;
 }
