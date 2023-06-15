bdbf07deff99d274df5fe5ab6c319af2b67daa75
Kitajima Akira <kitajima@osakac.ac.jp>
2023-06-15 13:33:12 +0900

    関数を導入した。

--- dom2019a/dom2019a2.cpp
+++ dom2019a/dom2019a2.cpp
@@ -8,6 +8,18 @@
 #include <vector>
 using namespace std;
 
+// データをもとに、合計値が1番大きい値を選ぶ。
+int get_answer(const vector<vector<int>>& table) {
+	// 生徒ごとの和を求める。
+	vector<int> sum(table[0].size(), 0);  // 生徒ごとの合計点
+	for (const auto& subject_data : table)
+		for (int i = 0; i < subject_data.size(); i++)
+			sum[i] += subject_data[i];
+
+	// 1番大きい値を返す。
+	return *max_element(sum.begin(), sum.end());
+}
+
 int main() {
 	for (int n, m; cin >> n >> m && !((n == 0) && (m == 0)); ) {// n 人数, m 科目数
 		// データを得る。
@@ -16,14 +28,8 @@ int main() {
 			for (auto& p : subject_data)
 				cin >> p;
 
-		// 生徒ごとの和を求める。
-		vector<int> sum(n, 0);  // 生徒ごとの合計点
-		for (const auto& subject_data : table)
-			for (int i = 0; i < n; i++)
-					sum[i] += subject_data[i];
-
-		// 1番大きい値を出力する。
-		cout << *max_element(sum.begin(), sum.end()) << endl;
+		// 解を求め、出力する。
+		cout << get_answer(table) << endl;
 	}
 	return 0;
 }
