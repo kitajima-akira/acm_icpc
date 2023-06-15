e8f2c41e27f188a189d4cbd41154c034a3eaf7d2
Kitajima Akira <kitajima@osakac.ac.jp>
2023-06-15 13:45:22 +0900

    コードを整理。

--- dom2019b/dom2019b.cpp
+++ dom2019b/dom2019b.cpp
@@ -13,11 +13,12 @@
 #include <vector>
 using namespace std;
 
-/** キーボード上の文字の位置を得る。
-*  @param keyboard キーボード
-*  @param c 探す文字
-*  @return 文字の位置の座標 (左上が(0, 0))
-*/
+/**
+ * キーボード上の文字の位置を得る。
+ *  @param keyboard キーボード
+ *  @param c 探す文字
+ *  @return 文字の位置の座標 (左上が(0, 0))
+ */
 pair<int, int> key_position(const vector<string>& keyboard, char c) {
 	int x = -1, y = -1;  // 求める位置の座標
 
@@ -30,7 +31,8 @@ pair<int, int> key_position(const vector<string>& keyboard, char c) {
 	return { x, y };
 }
 
-/** (x0, y0)から(x1, y1)までのマンハッタン距離を得る。
+/**
+ * (x0, y0)から(x1, y1)までのマンハッタン距離を得る。
  * @param x0 座標
  * @param y0 座標
  * @param x1 座標
@@ -41,7 +43,8 @@ int Manhattan_distance(int x0, int y0, int x1, int y1) {
 	return abs(x1 - x0) + abs(y1 - y0);
 }
 
-/** キーボードで何回入力したら文字列@a s が入力できるのか最小値を得る。
+/**
+ * キーボードで何回入力したら文字列@a s が入力できるのか最小値を得る。
  *  @param keyboard キーボード
  *  @param s 判定する文字列
  *  @return 最小値
@@ -63,21 +66,14 @@ int min_key_count(const vector<string>& keyboard, const string& s) {
 }
 
 int main() {
-	for (;;) {
-		// スクリーンキーボードの大きさを得る。
-		int h, w;  // スクリーンキーボードの高さ、幅
-		cin >> h >> w;
-
-		// 終了判定する。
-		if (h == 0 && w == 0)
-			break;
-
+	for (int h, w;  // スクリーンキーボードの高さ、幅
+		cin >> h >> w && (h > 0 || w > 0);) {
 		// キーボードを読み込む。
-		vector<string> keyboard;
-		for (int i = 0; i < h; i++) {
+		vector<string> keyboard(h);
+		for (auto& row : keyboard) {
 			string r;
 			cin >> r;
-			keyboard.push_back(r);
+			row = r;
 		}
 
 		// 判定に用いる文字列を得る。
